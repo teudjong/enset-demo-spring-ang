05838da4be3858c5f5c97406c019c02a9bf250c5
Teudjong Boumeu <gnilapon.ebenezaire@gmail.com>
2024-05-02 23:36:28 +0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
+
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
